57b259514dd0fa0df96bec34895e0215d34fbd1b
diegobarros <diegobarrosflf@gmail.com>
2020-11-18 20:38:28 -0300

    Criando projeto

---
+++ src/main/java/com/example/demo/ProjetoSpringDs1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringDs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringDs1Application.class, args);
+	}
+
+}
