8467e40babf0a76b0cddfe93ce5600a4d316b33b
diegobarros <diegobarrosflf@gmail.com>
2020-11-18 21:00:40 -0300

    Criando BaseEntity e NamedEntity.

---
+++ src/main/java/com/example/demo/model/NamedEntity.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+public abstract class NamedEntity extends BaseEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
